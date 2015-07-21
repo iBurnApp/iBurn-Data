fbb9325d701501f46589401330ef2ee48d302266
Chris Ballinger <chrisballinger@gmail.com>
2015-07-20 21:04:30 -0700

    Update 2015 data

--- data/2015/update.json.js
+++ data/2015/update.json.js
@@ -1,10 +1,10 @@
 {
 	"art": {"file": "art.json.js",
-			"updated": "2015-07-13 12:30:00"},
+			"updated": "2015-07-20 12:30:00"},
 	"camps": {"file": "camps.json.js",
-			  "updated": "2015-07-13 12:30:00"},
+			  "updated": "2015-07-20 12:30:00"},
 	"events": {"file": "events.json.js",
-			   "updated": "2015-07-13 12:30:00"},
+			   "updated": "2015-07-20 12:30:00"},
 	"tiles": {"file": "iburn.mbtiles.jar",
 			  "updated": "2015-06-25 16:30:00"}
 }
