165e8e866a30a7b5b23d021d87f17f474e94cfc7
Chris Ballinger <chrisballinger@gmail.com>
2015-08-04 23:38:12 -0700

    Remove camps from public repo :(

--- data/2015/update.json.js
+++ data/2015/update.json.js
@@ -2,7 +2,7 @@
 	"art": {"file": "art.json.js",
 			"updated": "2015-07-20 12:30:00"},
 	"camps": {"file": "camps.json.js",
-			  "updated": "2015-07-20 12:30:00"},
+			  "updated": "2015-08-04 12:30:00"},
 	"events": {"file": "events.json.js",
 			   "updated": "2015-07-20 12:30:00"},
 	"tiles": {"file": "iburn.mbtiles.jar",
