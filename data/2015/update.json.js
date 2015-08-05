87e75920f1cbf14502016252341284d5f9996c25
Chris Ballinger <chrisballinger@gmail.com>
2015-08-04 23:37:19 -0700

    Rename iburn.mbtiles to .jar extension

--- data/2015/update.json.js
+++ data/2015/update.json.js
@@ -6,5 +6,5 @@
 	"events": {"file": "events.json.js",
 			   "updated": "2015-07-20 12:30:00"},
 	"tiles": {"file": "iburn.mbtiles.jar",
-			  "updated": "2015-06-25 16:30:00"}
+			  "updated": "2015-08-04 16:30:00"}
 }
