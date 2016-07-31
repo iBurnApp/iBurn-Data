23c79a47b6a658ab3d5295117f19110040761a4c
Chris Ballinger <chrisballinger@gmail.com>
2016-07-31 15:20:27 -0700

    Print out geocoder failures in art.js

--- scripts/2015/art.js
+++ scripts/2015/art.js
@@ -2,22 +2,22 @@ var Geocoder = require("./geocoder/geocoder.js");
 var fs = require('fs');
 var turf = require('turf')
 
-var nopt = require("nopt")
-  , Stream = require("stream").Stream
-  , path = require("path")
-  , knownOpts = {
-    "layout": path,
-    "file" : path,
-    "key": String,
-    "out": path
-  }
-  , shortHands = {
-     "l" : ["--layout"],
-     "f" : ["--file"],
-     "o" : ["--out"],
-     "k" : ["--key"]
-   }
-  , parsed = nopt(knownOpts, shortHands, process.argv, 2)
+var nopt = require("nopt"),
+    Stream = require("stream").Stream,
+    path = require("path"),
+    knownOpts = {
+        "layout": path,
+        "file": path,
+        "key": String,
+        "out": path
+    },
+    shortHands = {
+        "l": ["--layout"],
+        "f": ["--file"],
+        "o": ["--out"],
+        "k": ["--key"]
+    },
+    parsed = nopt(knownOpts, shortHands, process.argv, 2)
 
 var layout = JSON.parse(fs.readFileSync(parsed.layout, 'utf8'));
 var art = JSON.parse(fs.readFileSync(parsed.file, 'utf8'));
@@ -26,18 +26,30 @@ var key = parsed.key;
 var coder = new Geocoder(layout);
 
 var result = [];
-art.map(function(item){
-  if (item[key]) {
-
-    var point = coder.forward(item[key])
-    item.latitude = point.geometry.coordinates[1];
-    item.longitude = point.geometry.coordinates[0];
+art.map(function(item) {
+    if (item[key]) {
+        var point = coder.forward(item[key])
+        if (point) {
+            var lat = point.geometry.coordinates[1];
+            var lon = point.geometry.coordinates[0];
+            if (typeof lat == "number" &&
+                typeof lon == "number") {
+                item.location.gps_latitude = lat;
+                item.location.gps_longitude = lon;                
+            } else {
+                console.log('non-number result ' + item.name + ' @ ' + item[key] + ': ' + item.location.gps_latitude + ' /// ' + item.location.gps_longitude)
+            }
+        } else {
+            console.log('could not geocode ' + item.name + ': ' + item[key])
+        }
+    } else {
+        console.log('missing key: ' + item.name)
+    }
     result.push(item);
-  }
 });
 
 if (parsed.out) {
-  fs.writeFile(parsed.out, JSON.stringify(result), function(err) {});
+    fs.writeFile(parsed.out, JSON.stringify(result), function(err) {});
 } else {
-  console.log(JSON.stringify(result));
+    console.log(JSON.stringify(result));
 }
