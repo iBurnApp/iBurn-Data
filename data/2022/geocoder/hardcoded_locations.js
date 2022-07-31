df4de0fd010aade2bc627939fba31760d8ae86c5
Chris Ballinger <chrisballinger@gmail.com>
2022-07-31 14:27:24 -0700

    2022 geocoder

---
+++ data/2022/geocoder/hardcoded_locations.js
@@ -0,0 +1,10 @@
+module.exports = {
+  // "Esplanade & 5:45": { 
+  //   type: 'Feature',
+  //   properties: {},
+  //   geometry:
+  //    { type: 'Point',
+  //      coordinates: [ -119.21134185791016, 40.78132629394531 ] 
+  //    } 
+  // },
+}
